1a1e24cf2d9417e6397c4c626251e46fb08b0a8e
EpicPlayerA10 <adasko.lesiak@gmail.com>
2026-05-02 23:55:37 +0200

    use latest shadcn

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,9 @@
 import type { Metadata } from "next";
-import { Poppins } from "next/font/google";
+import { Geist } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
-const font = Poppins({
-    subsets: ["latin"],
-    weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-});
+const geist = Geist({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
     title: "Create Next App",
@@ -18,8 +16,8 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="pl">
-            <body className={font.className}>{children}</body>
+        <html lang="pl" className={cn("font-sans", geist.variable)}>
+            <body>{children}</body>
         </html>
     );
 }
